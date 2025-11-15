caa6e589654f5249b68ecc2f2d37b7abfd57f1f3
Bruno Ferreira <bruno.ferreira.sa18@gmail.com>
2025-11-14 22:18:03 -0300

    feat: adicionado o total de seguidores e seguindo e código refatorado.

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,25 @@
+import { fetchGithubUser } from './githubApi.js';
+import { renderProfile } from './profileView.js';
+
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.querySelector('.profile-results');
 
-const BASE_URL = 'https://api.github.com';
-
 btnSearch.addEventListener('click', async () => {
     const userName = inputSearch.value;
-    if (userName) {
-        profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
-        
-        try {
-
-            const response = await fetch(`${BASE_URL}/users/${userName}`);
-
-            if (!response.ok) {
-                alert('Usuário não econtrado. Por favor, verifique o nome de usuário e tente novamente.');
-                return;
-            }
-
-            const userData = await response.json();
-            console.log(userData);
-
-            profileResults.innerHTML = `
-            <div class="profile-card">
-                <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
-                <div class="profile-info">
-                    <h2>${userData.name}</h2>
-                    <p>${userData.bio || 'Não possui bio cadastrada.'}
-                </div>
-            </div>`;
-
-        } catch (error) {
-            console.log('Erro ao buscar o perfil do usuário.', error);
-            alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
-
-        }
-
-    } else {
+    if (!userName) {
         alert('Por favor, digite um nome de usuário do GitHub');
+        profileResults.innerHTML = '';
+        return;
+    }
+    profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
+
+    try {
+        const userData = await fetchGithubUser(userName);
+        renderProfile(userData, profileResults);
+    } catch (error) {
+        console.log('Erro ao buscar o perfil do usuário.', error);
+        alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
+        profile.innerHTML = '';
     }
 });
